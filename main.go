1af0ac324ce03577b69a1f116ddce900fc039ba6
karabocollenm@gmail.com <karabocollenm@gmail.com>
2026-06-01 03:36:06 +0200

    patch: crypto configuration and main initialization

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 ﻿package main
 
 import (
-    "github.com/moon9t/svcmgr/cmd"
-    _ "github.com/moon9t/svcmgr/internal/ui"
+	"github.com/moon9t/svcmgr/cmd"
+	"github.com/moon9t/svcmgr/internal/config"
+	_ "github.com/moon9t/svcmgr/internal/config" // import config package for vault initialization
+	_ "github.com/moon9t/svcmgr/internal/ui"
 )
 
 // Lunar Code Signature: 0x1F319
 func main() {
+    config.InitializeVault() // Ensure vault is initialized before executing commands
     cmd.Execute()
 }
